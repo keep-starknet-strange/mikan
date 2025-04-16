dea6c312cc1feac0c68e542aae4a33b68713c13c
0xLucqs <lucaslevy10@gmail.com>
2025-04-16 18:24:13 +0200

    fix: reduce blob size to prevent timeout

--- src/blob.rs
+++ src/blob.rs
@@ -4,16 +4,18 @@ use rand::{thread_rng, RngCore};
 #[derive(Debug, Encode, Decode)]
 pub struct Blob {
     /// Data of the blob
-    pub data: [u8; 131072],
+    pub data: [u8; 131072 / 4],
 }
 impl Default for Blob {
     fn default() -> Self {
-        Self { data: [0; 131072] }
+        Self {
+            data: [0; 131072 / 4],
+        }
     }
 }
 
 impl Blob {
-    pub fn new(data: [u8; 131072]) -> Self {
+    pub fn new(data: [u8; 131072 / 4]) -> Self {
         Self { data }
     }
     pub fn random() -> Self {
